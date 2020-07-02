76cf037b68533639d9cf87bb1ae68319e7174bee
JonathanDotExe <jhocheneder@gmx.at>
2020-07-02 20:09:22 +0200

    Added wav data structures

--- midicube/wav.h
+++ midicube/wav.h
@@ -8,6 +8,24 @@
 #ifndef MIDICUBE_WAV_H_
 #define MIDICUBE_WAV_H_
 
+#include <cstdint>
+
+struct WAVHeader {
+	char chunkID[4];
+	std::uint32_t chunkSize;
+	char riffType[4];
+};
+
+struct WAVFormat {
+	char chunkID[4];
+	std::uint32_t chunkSize;
+	std::uint16_t wFormatTag;
+	std::uint16_t wChannels;
+	std::uint32_t dwSamplesPerSec;
+	std::uint32_t dwAvgBytesPerSec;
+	std::uint16_t wBlockAlign;
+	std::uint16_t wBitsPerSample;
+};
 
 
 
