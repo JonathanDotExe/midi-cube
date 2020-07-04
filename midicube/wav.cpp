f5c41124829f8f7d28bbb0160cd6e307580bae16
JonathanDotExe <jhocheneder@gmx.at>
2020-07-04 22:19:49 +0200

    Reading wav now works better

--- midicube/wav.cpp
+++ midicube/wav.cpp
@@ -10,6 +10,33 @@
 #include <iostream>
 #include <fstream>
 
+static void reverse (char arr[], std::size_t size) {
+	size_t half = size/2;
+	for (size_t i = 0; i < half; ++i) {
+		char ch = arr[i];
+		arr[i] = arr[size - 1 - i];
+		arr[size - 1 - i] = ch;
+	}
+}
+
+static std::int64_t twos_complement(std::uint64_t num, std::uint16_t bits) {
+	std::uint64_t sign_mask = 1 << (bits - 1);
+	if (!(num & sign_mask)) {
+		//Positive
+		return num;
+	}
+	//Negative
+	std::uint64_t mask = 0;
+	for (std::uint16_t i = 0; i < bits; ++i) {
+		mask += 1 << i;
+	}
+	num = num & mask;
+	num = !num;
+	num = num & mask;
+	num += 1;
+	num = num & mask;
+	return -num;
+}
 
 bool read_wav(WAVAudio& audio, std::string fname) {
 	std::ifstream in;
@@ -26,15 +53,20 @@ bool read_wav(WAVAudio& audio, std::string fname) {
 
 		//Samples
 		buffer = new char[format.wBlockAlign];
-		double max_value = 1 << format.wBitsPerSample;
+		double max_value = (std::uint32_t) (1 << format.wBitsPerSample);
+		std::cout << format.wBitsPerSample << " " << max_value << std::endl;
 		while (!in.eof()) {
 			in.read(buffer, format.wBlockAlign);
+			//Reverse buffer
+			reverse(buffer, sizeof(buffer));
 			//To int
 			std::uint64_t sample = 0;
-			for (size_t i = 0; i < format.wBlockAlign; ++i) {
-				sample += (std::uint32_t) buffer[format.wBlockAlign - 1 - i] << (i * 8);
+			for (size_t i = 0; i < format.wBitsPerSample; ++i) {
+				size_t bit = format.wBitsPerSample - 1 - i;
+				size_t byte = bit/8;
+				sample += (std::uint64_t) (buffer[byte] & (1 << (bit % 8))) << byte;
 			}
-			audio.samples.push_back(sample/max_value * 2 - 1);
+			audio.samples.push_back(twos_complement(sample, format.wBitsPerSample)/max_value * 2);
 		}
 	}
 	catch (const std::ifstream::failure& e) {
