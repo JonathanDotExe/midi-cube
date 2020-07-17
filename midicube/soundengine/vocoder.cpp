df1b28c94602a76220e7ca96708c1547d3a57ef1
JonathanDotExe <jhocheneder@gmx.at>
2020-07-17 21:01:35 +0200

    Worked further on vocoder

--- midicube/soundengine/vocoder.cpp
+++ midicube/soundengine/vocoder.cpp
@@ -27,39 +27,37 @@ void Vocoder::process_note_sample(std::array<double, OUTPUT_CHANNELS>& channels,
 }
 
 void Vocoder::process_sample(std::array<double, OUTPUT_CHANNELS>& channels, SampleInfo& info) {
+	double sample = 0;
 	//Vocoder
 	if (std::any_of(channels.begin(), channels.end(), [] (double s) {return s != 0;})) {
-		std::array<double, OUTPUT_CHANNELS> carrier = {};
-		std::array<double, OUTPUT_CHANNELS> modulator = {};
-		std::array<double, OUTPUT_CHANNELS> filtered = {};
+		double carrier = channels.at(0);
+		double modulator = 0;
+		sample += carrier * data.carrier_amp;
+		double filtered = 0;
 		double input = info.input_sample;
+		std::cout << info.input_sample << std::endl;
 		for (size_t i = 0; i < carrier_filters.size(); ++i) {
-			carrier = channels;
-			for (size_t j = 0; j < modulator.size(); ++j) {
-				modulator[j] = input;
-			}
-			carrier_filters[i].apply(carrier, info.time_step);
-			modulator_filters[i].apply(modulator, info.time_step);
-			for (size_t j = 0; j < filtered.size(); ++j) {
-				filtered[j] = carrier[j] * modulator[j];
-			}
+			carrier = channels.at(0);
+			modulator = input;
+			carrier = carrier_filters[i].apply(carrier, info.time_step);
+			modulator = modulator_filters[i].apply(modulator, info.time_step);
+			filtered += carrier * modulator;
 		}
-		channels = filtered;
-		for (size_t i = 0; i < channels.size(); ++i) {
-			channels[i] *= 0.3 * data.vocoder_amp;
-		}
+		sample += filtered * 0.3 * data.vocoder_amp;
 	}
 
 	//Voice
-	double voice = info.input_sample;
+	double voice = info.input_sample * data.voice_amp;
+	sample += voice;
 	if (data.delay)  {
 		unsigned int del = data.delay_time * info.sample_rate;
-		delay.add_sample(voice, del, data.delay_feedback, del, data.delay_mul);
+		delay.add_sample(sample, del, data.delay_feedback, del, data.delay_mul);
 	}
-	voice += delay.process();
-	voice *= data.voice_amp;
+	sample += delay.process();
+
+	channels = {};
 	for (size_t i = 0; i < channels.size(); ++i) {
-		channels[i] += voice;
+		channels[i] += sample;
 	}
 }
 
@@ -68,13 +66,22 @@ void Vocoder::control_change(unsigned int control, unsigned int value) {
 		data.delay = value > 0;
 	}
 	if (control == data.delay_time_control) {
-		data.delay_time = (double) value/127.0 * 10.0;
+		data.delay_time = value/127.0 * 10.0;
 	}
 	if (control == data.delay_feedback_control) {
 		data.delay_feedback = value/4;
 	}
 	if (control == data.delay_mul_control) {
-		data.delay_mul = (double) value/127;
+		data.delay_mul = value/127.0;
+	}
+	if (control == data.vocoder_amp_control) {
+		data.vocoder_amp = value/127.0;
+	}
+	if (control == data.voice_amp_control) {
+		data.voice_amp = value/127.0;
+	}
+	if (control == data.carrier_amp_control) {
+		data.carrier_amp = value/127.0;
 	}
 }
 
--- midicube/soundengine/vocoder.h
+++ midicube/soundengine/vocoder.h
@@ -10,22 +10,26 @@
 
 #include "soundengine.h"
 
-#define VOCODER_BAND_COUNT 30
+#define VOCODER_BAND_COUNT 40
 #define VOCODER_LOW_BAND 120
-#define VOCODER_HIGH_BAND 330
+#define VOCODER_HIGH_BAND 440
 
 struct VocoderData {
-	bool delay = false;
+	bool delay = true;
 	double delay_time = 0.5;
 	unsigned int delay_feedback = 4;
 	double delay_mul = 1;
-	double voice_amp = 0;
 	double vocoder_amp = 1;
+	double voice_amp = 0;
+	double carrier_amp = 0;
 
 	unsigned int delay_control = 9;
 	unsigned int delay_time_control = 1;
 	unsigned int delay_feedback_control = 2;
 	unsigned int delay_mul_control = 3;
+	unsigned int vocoder_amp_control = 4;
+	unsigned int voice_amp_control = 5;
+	unsigned int carrier_amp_control = 6;
 };
 
 class Vocoder : public SoundEngine {
@@ -33,8 +37,8 @@ class Vocoder : public SoundEngine {
 private:
 	VocoderData data;
 	DelayBuffer delay;
-	std::array<MultiChannelBandPassFilter, VOCODER_BAND_COUNT> carrier_filters;
-	std::array<MultiChannelBandPassFilter, VOCODER_BAND_COUNT> modulator_filters;
+	std::array<BandPassFilter, VOCODER_BAND_COUNT> carrier_filters;
+	std::array<BandPassFilter, VOCODER_BAND_COUNT> modulator_filters;
 
 public:
 	Vocoder();
