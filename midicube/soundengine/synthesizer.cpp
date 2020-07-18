d7a5a11ad83c49bee4969c59f5b40791cca29770
JonathanDotExe <jhocheneder@gmx.at>
2020-07-18 19:05:19 +0200

    Started implementing synthesizer

--- midicube/soundengine/synthesizer.cpp
+++ midicube/soundengine/synthesizer.cpp
@@ -8,11 +8,44 @@
 #include "synthesizer.h"
 
 Synthesizer::Synthesizer() {
-	// TODO Auto-generated constructor stub
+	preset = new SynthesizerPreset();
+
+	release_time = 0;
+	for (size_t i = 0; i < preset->oscilators; ++i) {
+		if (preset->oscilators[i].env.release > release_time) {
+			release_time = preset->oscilators[i].env.release;
+		}
+	}
+}
+
+void Synthesizer::process_note_sample(std::array<double, OUTPUT_CHANNELS>& channels, SampleInfo& info, TriggeredNote& note) {
 
 }
 
+void Synthesizer::process_sample(std::array<double, OUTPUT_CHANNELS>& channels, SampleInfo& info) {
+
+}
+
+void Synthesizer::control_change(unsigned int control, unsigned int value) {
+	//Control bindings
+	for (size_t i = 0; i < preset->control_bindings.size() ; ++i) {
+		OscilatorSlot* slot = preset->oscilators.at(preset->control_bindings[i].oscilator).osc;
+		double val = preset->control_bindings[i].from + (preset->control_bindings[i].to - preset->control_bindings[i].from) * value/127.0;
+		slot->set_property(preset->control_bindings[i].property, val);
+	}
+}
+
+bool Synthesizer::note_finished(SampleInfo& info, TriggeredNote& note) {
+	return !note.pressed && note.release_time + release_time < info.time;
+}
+
+std::string get_name() {
+	return "Synthesizer";
+}
+
+
 Synthesizer::~Synthesizer() {
-	// TODO Auto-generated destructor stub
+	delete preset;
+	preset = nullptr;
 }
 
--- midicube/soundengine/synthesizer.h
+++ midicube/soundengine/synthesizer.h
@@ -13,6 +13,11 @@
 #include "../envelope.h"
 #include "../synthesis.h"
 
+struct OscilatorEnvelope {
+	OscilatorSlot* osc;
+	ADSREnvelope env;
+};
+
 struct FilterOscilator {
 	Filter* filter;
 	size_t osc;
@@ -35,18 +40,39 @@ struct ControlBinding {
 };
 
 struct SynthesizerPreset {
-	std::vector<OscilatorSlot*> oscilators;
+	std::vector<OscilatorEnvelope> oscilators;
 	std::vector<FilterOscilator> filters;
 	std::vector<ADSREnvelope> envelopes;
 	std::vector<EnvelopeBinding> envelope_bindings;
 	std::vector<ControlBinding> control_bindings;
+
+	~SynthesizerPreset() {
+		for (size_t i = 0; i < oscilators.size() ; ++i) {
+			delete oscilators[i].osc;
+		}
+		for (size_t i = 0; i < filters.size() ; ++i) {
+			delete filters[i].filter;
+		}
+	}
 };
 
 class Synthesizer: public SoundEngine {
 private:
 	SynthesizerPreset* preset = nullptr;
+	double release_time;
 public:
 	Synthesizer();
+
+	void process_note_sample(std::array<double, OUTPUT_CHANNELS>& channels, SampleInfo& info, TriggeredNote& note);
+
+	void process_sample(std::array<double, OUTPUT_CHANNELS>& channels, SampleInfo& info);
+
+	void control_change(unsigned int control, unsigned int value);
+
+	bool note_finished(SampleInfo& info, TriggeredNote& note);
+
+	std::string get_name() = 0;
+
 	virtual ~Synthesizer();
 };
 
