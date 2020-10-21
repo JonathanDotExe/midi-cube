20761c2fd83e7a829817a1bf99e3332b11f6e1b4
JonathanDotExe <jhocheneder@gmx.at>
2020-10-21 09:55:56 +0200

    Added header for soundfont synth

---
+++ midicube/soundengine/sfsynth.h
@@ -0,0 +1,38 @@
+/*
+ * sfsynth.h
+ *
+ *  Created on: 21 Oct 2020
+ *      Author: jojo
+ */
+
+#ifndef MIDICUBE_SOUNDENGINE_SFSYNTH_H_
+#define MIDICUBE_SOUNDENGINE_SFSYNTH_H_
+
+#include "soundengine.h"
+#include <fluidsynth.h>
+
+class SoundFontSynth {
+
+private:
+	fluid_settings_t* settings = nullptr;
+	fluid_synth_t* synth = nullptr;
+
+public:
+	void midi_message(MidiMessage& msg, SampleInfo& info);
+
+	void press_note(SampleInfo& info, unsigned int note, double velocity);
+
+	void release_note(SampleInfo& info, unsigned int note);
+
+	void process_sample(std::array<double, OUTPUT_CHANNELS>& channels, SampleInfo& info);
+
+	virtual ~SoundFontSynth() {
+		delete_fluid_synth(synth);
+		delete_fluid_settings(settings);
+	};
+
+};
+
+
+
+#endif /* MIDICUBE_SOUNDENGINE_SFSYNTH_H_ */
