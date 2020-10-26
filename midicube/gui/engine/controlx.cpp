41a0fb370bf0a97ce3d684f74e1ef1a03bc8d854
JonathanDotExe <jhocheneder@gmx.at>
2020-10-26 19:53:14 +0100

    Worked on drawbar component

---
+++ midicube/gui/engine/controlx.cpp
@@ -0,0 +1,64 @@
+/*
+ * controlx.cpp
+ *
+ *  Created on: Oct 26, 2020
+ *      Author: jojo
+ */
+
+#include "controlx.h"
+
+
+//OrganDrawbar
+OrganDrawbar::OrganDrawbar(int value) {
+	this->value = value;
+	get_layout().padding_left = 2;
+	get_layout().padding_right = 2;
+	get_layout().padding_top = 2;
+	get_layout().padding_bottom = 2;
+
+	style.border_color = BLACK;
+	style.border_thickness = 1;
+	style.border_radius = 1;
+	style.fill_color =  RAYWHITE;
+	style.hover_color = LIGHTGRAY;
+
+	button_style.border_color = BLACK;
+	button_style.border_thickness = 1;
+	button_style.border_radius = 0.3f;
+	button_style.fill_color = LIGHTGRAY;
+	button_style.hover_color = GRAY;
+}
+
+void OrganDrawbar::draw(int parentX, int parentY, NodeEnv env) {
+	int val = value;
+	render_box(parentX + x + layout.padding_left, parentY + y, width - layout.padding_left - layout.padding_right, height * val/9, style, env.hovered == this);
+	render_box(parentX + x, parentY + y + height * val/9, width, height/9, button_style, env.hovered == this);
+}
+
+void OrganDrawbar::set_on_change(std::function<void (double)> on_change) {
+	this->on_change = on_change;
+}
+
+void OrganDrawbar::on_mouse_drag(int x_motion, int y_motion, MouseButtonType button, NodeEnv env) {
+	if (button == MouseButtonType::LEFT) {
+		double change = (double) y_motion/height;
+		double old_prog = progress;
+		progress -= change;
+		if (progress < 0) {
+			progress = 0;
+		}
+		else if (progress > 1) {
+			progress = 1;
+		}
+		if (progress != old_prog) {
+			if (on_change) {
+				on_change(scale.value(progress));
+			}
+			frame->request_redraw();
+		}
+	}
+}
+
+OrganDrawbar::~OrganDrawbar() {
+
+}
---
+++ midicube/gui/engine/controlx.h
@@ -0,0 +1,45 @@
+/*
+ * controlsx.h
+ *
+ *  Created on: Oct 26, 2020
+ *      Author: jojo
+ */
+
+#ifndef MIDICUBE_GUI_ENGINE_CONTROLX_H_
+#define MIDICUBE_GUI_ENGINE_CONTROLX_H_
+
+#include "core.h"
+#include "util.h"
+#include "container.h"
+#include "../../util.h"
+
+class OrganDrawbar : public StyleableNode {
+
+private:
+	std::function<void (int)> on_change;
+
+public:
+	BoxStyle style;
+	BoxStyle button_style;
+	double value;
+
+	OrganDrawbar(int value);
+
+	virtual void draw(int parentX, int parentY, NodeEnv env);
+
+	virtual void set_on_change(std::function<void (double)> on_change);
+
+	FOCUS_TRAVERSABLE
+
+	virtual void on_mouse_drag(int x_motion, int y_motion, MouseButtonType button, NodeEnv env);
+
+	virtual Vector get_content_size() {
+		return {40, 150};
+	}
+
+	virtual ~OrganDrawbar();
+
+};
+
+
+#endif /* MIDICUBE_GUI_ENGINE_CONTROLX_H_ */
