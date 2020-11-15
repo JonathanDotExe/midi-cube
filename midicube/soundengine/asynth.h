8796a6bc7ce8c76cf762eecda2c5cdb3cc42a5b4
JonathanDotExe <jhocheneder@gmx.at>
2020-11-15 17:47:50 +0100

    Added structure for analog synth preset

--- midicube/soundengine/asynth.h
+++ midicube/soundengine/asynth.h
@@ -12,21 +12,39 @@
 #include "../oscilator.h"
 
 #define ANALOG_OSCILATOR_COUNT 8
-#define ANALOG_OSCILATOR_COUNT 8
+#define ANALOG_MOD_ENV_COUNT 8
+#define ANALOG_LFO_COUNT 8
 
-class OscilatorEntity {
-public:
-	double volume;
+struct OscilatorEntity {
+	double volume = 0;
 	ADSREnvelope env{0.0, 0, 1, 0.0};
-	AnalogOscilatorBank<SOUND_ENGINE_POLYPHONY, 8> osc;
+	AnalogOscilatorData osc;
+};
 
+struct ModEnvelopeEntity {
+	double volume = 0;
+	ADSREnvelope env{0.0, 0, 1, 0.0};
+};
+
+struct LFOEnvelopeEntity {
+	double volume = 0;
+	double freq = 1;
+	AnalogWaveForm waveform = AnalogWaveForm::SAW_DOWN;
+};
+
+struct AnalogSynthPreset {
+	std::array<OscilatorEntity, ANALOG_LFO_COUNT> lfos;
+	std::array<OscilatorEntity, ANALOG_MOD_ENV_COUNT> mod_envs;
+	std::array<OscilatorEntity, ANALOG_OSCILATOR_COUNT> oscilators;
 };
 
 class AnalogSynth : public BaseSoundEngine {
 
 private:
-	std::array<OscilatorEntity, ANALOG_OSCILATOR_COUNT> operators;
+
+
 public:
+	AnalogSynthPreset preset;
 
 	AnalogSynth();
 
