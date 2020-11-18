92d4bb374a05fb7ff52bb57385488ee666f23401
JonathanDotExe <jhocheneder@gmx.at>
2020-11-18 17:00:40 +0100

    Fixed velocity modulation

--- midicube/soundengine/asynth.cpp
+++ midicube/soundengine/asynth.cpp
@@ -8,9 +8,10 @@
 
 
 AnalogSynth::AnalogSynth() {
-	preset.oscilators.at(0).volume = 1;
-	preset.oscilators.at(0).env = {0.0005, 0, 1, 0.003};
-	preset.oscilators.at(0).unison_amount = 2;
+	OscilatorEntity& osc = preset.oscilators.at(0);
+	osc.volume = 1;
+	osc.env = {0.0005, 0, 1, 0.003};
+	osc.unison_amount = 2;
 }
 
 static double apply_modulation(const FixedScale& scale, PropertyModulation& mod, std::array<double, ANALOG_MOD_ENV_COUNT>& env_val, std::array<double, ANALOG_LFO_COUNT>& lfo_val, double velocity) {
@@ -21,13 +22,15 @@ static double apply_modulation(const FixedScale& scale, PropertyModulation& mod,
 }
 
 void AnalogSynth::process_note_sample(std::array<double, OUTPUT_CHANNELS>& channels, SampleInfo& info, TriggeredNote& note, KeyboardEnvironment& env, size_t note_index) {
-	double velocity = note.velocity/127.0;
 	//Mod Envs
 	std::array<double, ANALOG_MOD_ENV_COUNT> env_val;
 	for (size_t i = 0; i < preset.mod_envs.size(); ++i) {
 		ModEnvelopeEntity& mod_env = preset.mod_envs[i];
 		if (mod_env.volume) {
-			env_val.at(i) += mod_env.env.amplitude(info.time, note, env) * mod_env.volume;
+			env_val.at(i) = mod_env.env.amplitude(info.time, note, env) * mod_env.volume;
+		}
+		else {
+			env_val.at(i) = 0;
 		}
 	}
 	//LFOs
@@ -36,7 +39,10 @@ void AnalogSynth::process_note_sample(std::array<double, OUTPUT_CHANNELS>& chann
 		LFOEntity& lfo = preset.lfos[i];
 		if (lfo.volume) {
 			AnalogOscilatorData d = {lfo.waveform};
-			lfo_val.at(i) += lfos.at(i).signal(lfo.freq, info.time_step, d, false) * lfo.volume;
+			lfo_val.at(i) = lfos.at(i).signal(lfo.freq, info.time_step, d, false) * lfo.volume;
+		}
+		else {
+			lfo_val.at(i) = 0;
 		}
 	}
 	//Synthesize
@@ -47,9 +53,9 @@ void AnalogSynth::process_note_sample(std::array<double, OUTPUT_CHANNELS>& chann
 			AnalogOscilatorData data = {osc.waveform, osc.analog, osc.sync};
 			AnalogOscilatorBankData bdata = {0.1, osc.unison_amount};
 			//Apply modulation
-			data.sync_mul = apply_modulation(SYNC_SCALE, osc.sync_mul, env_val, lfo_val, velocity);
-			data.pulse_width = apply_modulation(PULSE_WIDTH_SCALE, osc.pulse_width, env_val, lfo_val, velocity);
-			bdata.unison_detune = apply_modulation(UNISON_DETUNE_SCALE, osc.unison_detune, env_val, lfo_val, velocity);
+			data.sync_mul = apply_modulation(SYNC_SCALE, osc.sync_mul, env_val, lfo_val, note.velocity);
+			data.pulse_width = apply_modulation(PULSE_WIDTH_SCALE, osc.pulse_width, env_val, lfo_val, note.velocity);
+			bdata.unison_detune = apply_modulation(UNISON_DETUNE_SCALE, osc.unison_detune, env_val, lfo_val, note.velocity);
 
 			sample += oscilators.signal(note.freq, info.time_step, note_index + i * SOUND_ENGINE_POLYPHONY, data, bdata, false) * osc.volume;
 		}
