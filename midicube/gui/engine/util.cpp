47a0b40cef1026fab03b22bcc189a0d506f13c61
JonathanDotExe <jhocheneder@gmx.at>
2020-12-20 18:20:11 +0100

    Button text is now centered correctly

--- midicube/gui/engine/util.cpp
+++ midicube/gui/engine/util.cpp
@@ -8,9 +8,8 @@
 #include "util.h"
 
 void center_text(sf::Text& text, int x, int y, int width, int height) {
-	int w = text.getLocalBounds().width;
-	int h = text.getLocalBounds().height;
-
-	text.setPosition(x + width/2 - w/2, y + height/2 - h/2);
+	sf::FloatRect bounds = text.getLocalBounds();
+	text.setOrigin(bounds.left + bounds.width/2,  bounds.top + bounds.height/2);
+	text.setPosition(x + width/2, y + height/2);
 }
 
