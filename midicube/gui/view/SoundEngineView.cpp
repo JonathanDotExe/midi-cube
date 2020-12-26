b4ec785d55f82db1388b09d877b882a86514fa31
JonathanDotExe <jhocheneder@gmx.at>
2020-12-26 14:35:33 +0100

    Add panes for each sound engine

--- midicube/gui/view/SoundEngineView.cpp
+++ midicube/gui/view/SoundEngineView.cpp
@@ -8,6 +8,7 @@
 #include "SoundEngineView.h"
 
 #include <iostream>
+#include "../../soundengine/soundengine.h"
 
 SoundEngineView::SoundEngineView() : ViewController() {
 
@@ -16,7 +17,24 @@ SoundEngineView::SoundEngineView() : ViewController() {
 std::vector<Control*> SoundEngineView::create(Frame& frame) {
 	std::vector<Control*> controls;
 
-	//Pane
+	//Background
+	Pane* bg = new Pane(sf::Color(80, 80, 80), 0, 0, frame.get_width(), frame.get_height());
+	controls.push_back(bg);
+
+	//Channels
+	int rows = 2;
+	int cols = SOUND_ENGINE_MIDI_CHANNELS / rows;
+	int pane_width = (frame.get_width() - 15) / cols;
+	int pane_height = (frame.get_height() - 50 - 5) / rows;
+	for (size_t i = 0; i < SOUND_ENGINE_MIDI_CHANNELS; ++i) {
+		int x = 10 + pane_width * (i % cols);
+		int y = 10 + pane_height * (i / cols);
+		Pane* pane = new Pane(sf::Color(120, 120, 120), x, y, pane_width - 5, pane_height - 5);
+
+		controls.push_back(pane);
+	}
+
+	/*//Pane
 	Pane* pane = new Pane(sf::Color(220, 220, 220), 0, 250, 300, 300);
 	controls.push_back(pane);
 
@@ -44,7 +62,7 @@ std::vector<Control*> SoundEngineView::create(Frame& frame) {
 
 	//Check Box
 	CheckBox* check_box = new CheckBox(true, "Demo Checkbox", main_font, 12, 250, 10, 30, 30);
-	controls.push_back(check_box);
+	controls.push_back(check_box);*/
 
 	return controls;
 }
