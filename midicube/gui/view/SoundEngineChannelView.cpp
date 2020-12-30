50219e63e00fa43fc82afbc6b47186418fbfe7d2
JonathanDotExe <jhocheneder@gmx.at>
2020-12-30 13:14:47 +0100

    Added checkboxes for midi filtering

--- midicube/gui/view/SoundEngineChannelView.cpp
+++ midicube/gui/view/SoundEngineChannelView.cpp
@@ -42,7 +42,7 @@ Scene SoundEngineChannelView::create(Frame &frame) {
 
 	//Col 1
 	//Engine
-	ComboBox* engine = new ComboBox(0, engine_names, main_font, 18, -1, 10, 45, 200, 40);
+	ComboBox* engine = new ComboBox(0, engine_names, main_font, 24, -1, 10, 45, 300, 80);
 	engine->rect.setFillColor(sf::Color(0, 180, 255));
 	engine->bind(&channel, SoundEngineChannelProperty::pChannelSoundEngine);
 	controls.push_back(engine);
@@ -50,50 +50,108 @@ Scene SoundEngineChannelView::create(Frame &frame) {
 	//Col 2
 	//Volume
 	{
-		Label* vol_label = new Label("Vol.", main_font, 24, 230, 10);
+		Label* vol_label = new Label("Vol.", main_font, 24, 330, 10);
 		controls.push_back(vol_label);
 
-		Slider* volume = new Slider(0, 0, 1, main_font, 230, 45, 40, 300, 0.7, 0.1);
+		Slider* volume = new Slider(0, 0, 1, main_font, 330, 45, 60, 400, 0.7, 0.1);
 		volume->bind(&channel, SoundEngineChannelProperty::pChannelVolume);
 		controls.push_back(volume);
 	}
 	//Panning
 	{
-		Label* pan_label = new Label("Pan.", main_font, 24, 290, 10);
+		Label* pan_label = new Label("Pan.", main_font, 24, 400, 10);
 		controls.push_back(pan_label);
 
-		Slider* pan = new Slider(0, 0, 1, main_font, 290, 45, 40, 300, 0.7, 0.1);
+		Slider* pan = new Slider(0, 0, 1, main_font, 400, 45, 60, 400, 0.7, 0.1);
 		//pan->bind(&channel, SoundEngineChannelProperty::pChannelVolume); TODO implement channel pan
 		controls.push_back(pan);
 	}
 
+	int tmp_y = 10;
 	//Col 3
 	//Source
 	{
-		Label* source_label = new Label("Source", main_font, 18, 350, 15);
+		tmp_y += 5;
+		Label* source_label = new Label("Source", main_font, 18, 470, tmp_y);
+		tmp_y += 30;
 		controls.push_back(source_label);
 
-		DragBox<int>* source = new DragBox<int>(0, -1, 15, main_font, 18, 350, 45, 100, 40);
+		DragBox<int>* source = new DragBox<int>(0, -1, 15, main_font, 18, 470, tmp_y, 150, 60);
 		source->bind(&channel, SoundEngineChannelProperty::pChannelInputDevice);
+		tmp_y += 60;
 		controls.push_back(source);
 	}
 	//Channel
 	{
-		Label* channel_label = new Label("Channel", main_font, 18, 350, 90);
+		tmp_y += 5;
+		Label* channel_label = new Label("Channel", main_font, 18, 470, tmp_y);
+		tmp_y += 30;
 		controls.push_back(channel_label);
 
-		DragBox<int>* input_channel = new DragBox<int>(0, 0, 15, main_font, 18, 350, 120, 100, 40);
+		DragBox<int>* input_channel = new DragBox<int>(0, 0, 15, main_font, 18, 470, tmp_y, 150, 60);
 		input_channel->bind(&channel, SoundEngineChannelProperty::pChannelInputChannel);
+		tmp_y += 60;
 		controls.push_back(input_channel);
 	}
 	//Octave
 	{
-		Label* octave_label = new Label("Octave", main_font, 18, 350, 165);
+		tmp_y += 5;
+		Label* octave_label = new Label("Octave", main_font, 18, 470, tmp_y);
+		tmp_y += 30;
 		controls.push_back(octave_label);
 
-		DragBox<int>* octave = new DragBox<int>(0, -3, 3, main_font, 18, 350, 195, 100, 40);
+		DragBox<int>* octave = new DragBox<int>(0, -3, 3, main_font, 18, 470, tmp_y, 150, 60);
+		tmp_y += 60;
 		octave->bind(&channel, SoundEngineChannelProperty::pChannelOctave);
 		controls.push_back(octave);
+	}
+
+	//Col 4
+	tmp_y = 10;
+	Label* midi_filter = new Label("MIDI-Filter", main_font, 24, 630, 10);
+	controls.push_back(midi_filter);
+	tmp_y += 30;
+	//CC
+	{
+		tmp_y += 5;
+		CheckBox* cc = new CheckBox(true, "CC", main_font, 18, 630, tmp_y, 40, 40);
+		cc->bind(&channel, SoundEngineChannelProperty::pChannelTransferCC);
+		tmp_y += 40;
+		controls.push_back(cc);
+	}
+	//Pitch
+	{
+		tmp_y += 20;
+		CheckBox* pitch = new CheckBox(true, "Pitch", main_font, 18, 630, tmp_y, 40, 40);
+		pitch->bind(&channel, SoundEngineChannelProperty::pChannelTransferPitchBend);
+		tmp_y += 40;
+		controls.push_back(pitch);
+	}
+	//Prog
+	{
+		tmp_y += 20;
+		CheckBox* pitch = new CheckBox(true, "Prog", main_font, 18, 630, tmp_y, 40, 40);
+		pitch->bind(&channel, SoundEngineChannelProperty::pChannelTransferProgChange);
+		tmp_y += 40;
+		controls.push_back(pitch);
+	}
+	tmp_y = 45;
+
+	//Aftertouch
+	{
+		tmp_y += 5;
+		CheckBox* cc = new CheckBox(true, "Aftertouch", main_font, 18, 780, tmp_y, 40, 40);
+		cc->bind(&channel, SoundEngineChannelProperty::pChannelTransferCC);
+		tmp_y += 40;
+		controls.push_back(cc);
+	}
+	//Other
+	{
+		tmp_y += 20;
+		CheckBox* pitch = new CheckBox(true, "Other", main_font, 18, 780, tmp_y, 40, 40);
+		pitch->bind(&channel, SoundEngineChannelProperty::pChannelTransferOther);
+		tmp_y += 40;
+		controls.push_back(pitch);
 	}
 
 	//Back Button
