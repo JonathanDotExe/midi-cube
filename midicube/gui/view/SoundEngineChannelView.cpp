38bdcccce16263c26d5b691d844e6529754f1edf
JonathanDotExe <jhocheneder@gmx.at>
2020-12-30 11:52:01 +0100

    Added combo box for sound engine

--- midicube/gui/view/SoundEngineChannelView.cpp
+++ midicube/gui/view/SoundEngineChannelView.cpp
@@ -20,6 +20,13 @@ Scene SoundEngineChannelView::create(Frame &frame) {
 	std::vector<Control*> controls;
 	std::vector<PropertyHolder*> holders;
 
+	//Sound engines
+	std::vector<std::string> engine_names;
+	engine_names.push_back("None");
+	for (SoundEngineBank* engine : frame.cube.engine.get_sound_engines()) {
+		engine_names.push_back(engine->get_name());
+	}
+
 	//Background
 	Pane* bg = new Pane(sf::Color(80, 80, 80), 0, 0, frame.get_width(), frame.get_height());
 	controls.push_back(bg);
@@ -28,9 +35,14 @@ Scene SoundEngineChannelView::create(Frame &frame) {
 	controls.push_back(pane);
 
 	//Title
-	Label* title = new Label("Channel " + std::to_string(channel_index + 1), main_font, 18, 5, 5);
+	Label* title = new Label("Channel " + std::to_string(channel_index + 1), main_font, 18, 10, 10);
 	controls.push_back(title);
 
+	//Engine
+	ComboBox* engine = new ComboBox(0, engine_names, main_font, 12, -1, 10, 40, 200, 40);
+	engine->rect.setFillColor(sf::Color(0, 180, 255));
+	engine->bind(&channel, SoundEngineChannelProperty::pChannelSoundEngine);
+	controls.push_back(engine);
 	//Back Button
 	Button* back = new Button("Back", main_font, 18, frame.get_width() - 70, frame.get_height() - 40, 70, 40);
 	back->set_on_click([&frame]() {
