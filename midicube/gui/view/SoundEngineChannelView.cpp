b70cb6d82bbc654d3133cb8e4d7a5c2942289992
JonathanDotExe <jhocheneder@gmx.at>
2020-12-30 11:46:44 +0100

    Added back button in sound engine channel view

--- midicube/gui/view/SoundEngineChannelView.cpp
+++ midicube/gui/view/SoundEngineChannelView.cpp
@@ -7,6 +7,7 @@
 
 #include "SoundEngineChannelView.h"
 #include "resources.h"
+#include "SoundEngineView.h"
 
 SoundEngineChannelView::SoundEngineChannelView(SoundEngineChannel& ch, int channel_index) : channel(ch) {
 	this->channel_index = channel_index;
@@ -23,10 +24,19 @@ Scene SoundEngineChannelView::create(Frame &frame) {
 	Pane* bg = new Pane(sf::Color(80, 80, 80), 0, 0, frame.get_width(), frame.get_height());
 	controls.push_back(bg);
 
+	Pane* pane = new Pane(sf::Color(120, 120, 120), 5, 5, frame.get_width() - 10, frame.get_height() - 50);
+	controls.push_back(pane);
+
 	//Title
 	Label* title = new Label("Channel " + std::to_string(channel_index + 1), main_font, 18, 5, 5);
 	controls.push_back(title);
 
+	//Back Button
+	Button* back = new Button("Back", main_font, 18, frame.get_width() - 70, frame.get_height() - 40, 70, 40);
+	back->set_on_click([&frame]() {
+		frame.change_view(new SoundEngineView());
+	});
+	controls.push_back(back);
 
 	return {controls, holders};
 }
