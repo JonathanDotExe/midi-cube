9f0d038da903157852fdcd12d26b61263f856071
JonathanDotExe <jhocheneder@gmx.at>
2021-02-17 18:35:43 +0100

    Added scroll buttons in program view

--- midicube/gui/view/ProgramView.cpp
+++ midicube/gui/view/ProgramView.cpp
@@ -32,8 +32,10 @@ Scene ProgramView::create(Frame &frame) {
 		Pane* pane = new Pane(sf::Color(120, 120, 120), 5, 5, frame.get_width() - 10, frame.get_height() - 50);
 		controls.push_back(pane);
 
+		Bank* bank = prog_mgr->get_bank(this->bank);
+
 		//Title
-		Label* title = new Label("Programs", main_font, 24, 10, 10);
+		Label* title = new Label(bank->name + " - Page " + std::to_string(page + 1), main_font, 24, 10, 10);
 		controls.push_back(title);
 
 		//Programs
@@ -42,15 +44,13 @@ Scene ProgramView::create(Frame &frame) {
 		int pane_width = (frame.get_width() - 15) / cols;
 		int pane_height = pane_width;
 
-		Bank* bank = prog_mgr->get_curr_bank();
-
 		const size_t start = page * rows * cols;
 		const size_t size = std::min((size_t) (bank->programs.size() - start), (size_t) (rows * cols));
 		for (size_t i = 0; i < size; ++i) {
 			Program* prog = bank->programs[start + i];
 
 			int x = 10 + pane_width * (i % cols);
-			int y = 10 + pane_height * (i / cols);
+			int y = 50 + pane_height * (i / cols);
 
 			//Button
 			Button* button = new Button(prog->name, main_font, 16, x, y,  pane_width - 5, pane_height - 5);
@@ -63,11 +63,41 @@ Scene ProgramView::create(Frame &frame) {
 			});
 		}
 
+		//Previous bank
+		Button* previous_bank = new Button("<<", main_font, 18, 0, frame.get_height() - 40, 60, 40);
+		previous_bank->set_on_click([&frame, this]() {
+			frame.change_view(new ProgramView(this->bank - 1, 0));
+		});
+		previous_bank->rect.setFillColor(sf::Color(200, 200, 200));
+		controls.push_back(previous_bank);
+		//Previous page
+		Button* previous_page = new Button("<", main_font, 18, 60, frame.get_height() - 40, 60, 40);
+		previous_page->set_on_click([&frame, this]() {
+			frame.change_view(new ProgramView(this->bank, page - 1));
+		});
+		controls.push_back(previous_page);
+
+		//Next page
+		Button* next_page = new Button(">", main_font, 18, frame.get_width() - 70 - 60 * 2, frame.get_height() - 40, 60, 40);
+		next_page->set_on_click([&frame, this]() {
+			frame.change_view(new ProgramView(this->bank, page + 1));
+		});
+		controls.push_back(next_page);
+
+		//Next bank
+		Button* next_bank = new Button(">>", main_font, 18, frame.get_width() - 70 - 60, frame.get_height() - 40, 60, 40);
+		next_bank->set_on_click([&frame, this]() {
+			frame.change_view(new ProgramView(this->bank + 1, 0));
+		});
+		next_bank->rect.setFillColor(sf::Color(200, 200, 200));
+		controls.push_back(next_bank);
+
 		//Back Button
 		Button* back = new Button("Back", main_font, 18, frame.get_width() - 70, frame.get_height() - 40, 70, 40);
 		back->set_on_click([&frame]() {
 			frame.change_view(new SoundEngineView());
 		});
+		back->rect.setFillColor(sf::Color::Yellow);
 		controls.push_back(back);
 	}
 	prog_mgr->unlock();
