264e454b5e2f05bbb617acea20f3b8397e5074a3
JonathanDotExe <jhocheneder@gmx.at>
2021-02-22 17:30:38 +0100

    Refactored filter algorithm

--- midicube/filter.cpp
+++ midicube/filter.cpp
@@ -14,8 +14,8 @@
 const double TWO_POLE_FACTOR = 1.0/sqrt(pow(2, 1/2.0) - 1);
 const double FOUR_POLE_FACTOR = 1.0/sqrt(pow(2, 1/4.0) - 1);
 
-const double HIGHPASS_TWO_POLE_FACTOR = 1.0/sqrt(pow(2, 1/2.0) - 1);
-const double HIGHPASS_FOUR_POLE_FACTOR = 1.0/sqrt(pow(2, 1/4.0) - 1);
+const double HIGHPASS_TWO_POLE_FACTOR = sqrt(pow(2, 1/2.0) - 1);
+const double HIGHPASS_FOUR_POLE_FACTOR = sqrt(pow(2, 1/4.0) - 1);
 
 /**
  * To convert frequency into filter cutoff use time_step/(rc+time_step)
@@ -23,45 +23,58 @@ const double HIGHPASS_FOUR_POLE_FACTOR = 1.0/sqrt(pow(2, 1/4.0) - 1);
 
 //Filter
 double Filter::apply (FilterData& data, double sample, double time_step) {
-	//Cutoff pole factor
-	double factor = 1;
-	double cutoff = 0;
 	switch (data.type) {
-	case FilterType::LP_12:
-	case FilterType::BP_12:
-		factor = TWO_POLE_FACTOR;
-		cutoff = cutoff_to_factor(data.cutoff * factor, time_step);
-		break;
-	case FilterType::LP_24:
-	case FilterType::BP_24:
-		factor = FOUR_POLE_FACTOR;
-		cutoff = cutoff_to_factor(data.cutoff * factor, time_step);
-		break;
-	case FilterType::HP_12:
-		factor = HIGHPASS_TWO_POLE_FACTOR;
-		cutoff = cutoff_to_highpass_factor(data.cutoff * factor, time_step);
-		break;
-	case FilterType::HP_24:
-		factor = HIGHPASS_TWO_POLE_FACTOR;
-		cutoff = cutoff_to_highpass_factor(data.cutoff * factor, time_step);
-		break;
-	}
-
-	double feedback = data.resonance + data.resonance/(1 - cutoff);
-	//Update buffers
-	switch (data.type) {
+	//Low pass algorithm
 	case FilterType::LP_12:
 	case FilterType::LP_24:
 	case FilterType::BP_12:
 	case FilterType::BP_24:
-		//Low-pass poles
+	{
+		//Cutoff
+		double factor = 1;
+		if (data.type % 2 == 0) {
+			factor = TWO_POLE_FACTOR;
+		}
+		else {
+			factor = FOUR_POLE_FACTOR;
+		}
+		double cutoff = cutoff_to_factor(data.cutoff * factor, time_step);
+		double feedback = data.resonance + data.resonance/(1 - cutoff);
+		//Update buffer
 		pole1 += cutoff * (sample - pole1 + feedback * (pole1 - pole2));
 		pole2 += cutoff * (pole1 - pole2);
 		pole3 += cutoff * (pole2 - pole3);
 		pole4 += cutoff * (pole3 - pole4);
+
+		//Values
+		switch (data.type) {
+		case FilterType::LP_12:
+			return pole2;
+		case FilterType::LP_24:
+			return pole4;
+		case FilterType::BP_12:
+			return pole1 - pole2;
+		case FilterType::BP_24:
+			return pole1 - pole4;
+		default:
+			break;
+		}
+	}
 		break;
+	//High pass algorithm
 	case FilterType::HP_12:
 	case FilterType::HP_24:
+	{
+		//Cutoff
+		double factor = 1;
+		if (data.type % 2 == 0) {
+			factor = TWO_POLE_FACTOR;
+		}
+		else {
+			factor = FOUR_POLE_FACTOR;
+		}
+		double cutoff = cutoff_to_highpass_factor(data.cutoff * factor, time_step);
+		double feedback = data.resonance + data.resonance/(1 - cutoff);
 		//High pass poles
 		pole1 = cutoff * (sample - last_pole1 + pole1 + feedback * (pole1 - pole2));
 		pole2 = cutoff * (pole1 - last_pole2 + pole2);
@@ -72,20 +85,18 @@ double Filter::apply (FilterData& data, double sample, double time_step) {
 		last_pole2 = pole2;
 		last_pole3 = pole3;
 		last_pole4 = pole4;
+
+		switch (data.type) {
+		case FilterType::HP_12:
+			return pole2;
+		case FilterType::HP_24:
+			return pole4;
+		default:
+			break;
+		}
 	}
-	switch (data.type) {
-	case FilterType::LP_12:
-		return pole2;
-	case FilterType::LP_24:
-		return pole4;
-	case FilterType::HP_12:
-		return sample - pole2;
-	case FilterType::HP_24:
-		return sample - pole4;
-	case FilterType::BP_12:
-		return pole1 - pole2;
-	case FilterType::BP_24:
-		return pole1 - pole4;
+		break;
 	}
+
 	return sample;
 }
