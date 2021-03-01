79b1aa2fb8a82dc7bac9953727ac63e61079bda9
JonathanDotExe <jhocheneder@gmx.at>
2021-03-01 09:53:25 +0100

    Added more variables

--- midicube/autosampler.cpp
+++ midicube/autosampler.cpp
@@ -5,6 +5,16 @@
  *      Author: jojo
  */
 
+#include "autosampler.h"
 
+void AutoSampler::request_params() {
+}
 
+void AutoSampler::init() {
+}
 
+inline int AutoSampler::process(double *output_buffer, double *input_buffer,
+		unsigned int buffer_size, double time) {
+
+	return 0;
+}
--- midicube/autosampler.h
+++ midicube/autosampler.h
@@ -8,15 +8,33 @@
 #ifndef MIDICUBE_AUTOSAMPLER_H_
 #define MIDICUBE_AUTOSAMPLER_H_
 
+#include <iostream>
 #include <vector>
+#include <atomic>
 #include <array>
 #include <string>
+#include <rtmidi/RtMidi.h>
+#include <rtaudio/RtAudio.h>
 
 class AutoSampler {
-public:
+private:
+	RtAudio audio;
+	RtMidiOut rtmidi;
+
 	unsigned int channel = 0;
 	std::vector<unsigned int> notes;
 	std::vector<unsigned int> velocities;
+
+	unsigned int curr_note = 0;
+	unsigned int curr_velocity = 0;
+
+	std::atomic<bool> started = false;
+
+public:
+
+	void request_params();
+
+	void init();
 
 	inline int process(double* output_buffer, double* input_buffer, unsigned int buffer_size, double time);
 
