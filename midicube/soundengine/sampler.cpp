9662b3b41dd19378df0d02368275d8c464b0c7a4
JonathanDotExe <jhocheneder@gmx.at>
2021-03-15 20:50:27 +0100

    Loading sustain sample files

--- midicube/soundengine/sampler.cpp
+++ midicube/soundengine/sampler.cpp
@@ -228,6 +228,10 @@ extern SampleSound* load_sound(std::string folder) {
 				if (!read_audio_file(zone->sample, file)) {
 					std::cerr << "Couldn't load sample file " << file << std::endl;
 				}
+				std::string sfile = folder + "/" + z.second.get<std::string>("sustain_sample", "");
+				if (!read_audio_file(zone->sustain_sample, sfile)) {
+					std::cerr << "Couldn't load sample file " << file << std::endl;
+				}
 				layer->zones.push_back(zone);
 			}
 			sound->samples.push_back(layer);
