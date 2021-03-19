b8b8982911636491fafd732640fe0fe7a7e89c37
JonathanDotExe <jhocheneder@gmx.at>
2021-03-19 11:53:09 +0100

    Finished chorus

--- midicube/effect/chorus.cpp
+++ midicube/effect/chorus.cpp
@@ -17,7 +17,16 @@ void ChorusEffect::apply(double& lsample, double& rsample, ChorusPreset& preset,
 		double l = ldelay.process();
 		double r = rdelay.process();
 
+		//Delay
+		AnalogOscilatorData data = {preset.vibrato_waveform};
+		osc.process(preset.vibrato_rate, info.time_step, data);
+		double del = (1 + osc.modulator(preset.vibrato_rate, info.time_step, data) * preset.vibrato_depth) * preset.delay;
+		ldelay.add_isample(lsample, del);
+		rdelay.add_isample(rsample, del);
 
+		//Mix
+		lsample = lsample * (1 - preset.mix) + l * preset.mix;
+		rsample = rsample * (1 - preset.mix) + r * preset.mix;
 	}
 }
 
