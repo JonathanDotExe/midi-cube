36d4fc8cc611170d90d60f745155696500c51235
JonathanDotExe <jhocheneder@gmx.at>
2021-03-19 21:04:48 +0100

    Made some changes to the organ

--- midicube/effect/amplifier_simulation.cpp
+++ midicube/effect/amplifier_simulation.cpp
@@ -34,7 +34,7 @@ static double apply_distortion(double sample, double drive, DistortionType type)
 		break;
 	case DistortionType::ARCTAN_DISTORTION:
 	{
-		sample = 2 / M_PI * atan(sample * (1 + drive * 2.5));
+		sample = 2 / M_PI * atan(sample * (1.5 + drive * 2));
 	}
 		break;
 	}
