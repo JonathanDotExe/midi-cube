b0fed3e53b1c2d392de010d9cf565aed510f83f0
JonathanDotExe <jhocheneder@gmx.at>
2021-03-31 12:12:31 +0200

    Added effect builder

--- midicube/effect/effect.h
+++ midicube/effect/effect.h
@@ -61,6 +61,35 @@ public:
 	~MasterEffect();
 };
 
+class EffectBuilder {
+public:
+	virtual Effect* build() = 0;
 
+	virtual std::string get_name() = 0;
+
+	virtual bool matches(Effect* effect) = 0;
+
+	virtual ~EffectBuilder() {
+
+	};
+};
+
+template <typename T>
+std::string get_effect_name();
+
+template <typename T>
+class TemplateEffectBuilder : public EffectBuilder {
+public:
+	inline Effect* build() {
+		return new T();
+	}
+	std::string get_name() {
+		return get_effect_name<T>();
+	}
+
+	bool matches(Effect* effect) {
+		return dynamic_cast<T*>(effect) != nullptr;
+	}
+};
 
 #endif /* MIDICUBE_EFFECT_EFFECT_H_ */
