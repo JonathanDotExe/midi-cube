61537b2d367d660163ea9455da789f42c505d3a7
JonathanDotExe <jhocheneder@gmx.at>
2021-04-11 14:27:38 +0200

    Added basic master effect view

---
+++ midicube/gui/view/MasterEffectView.cpp
@@ -0,0 +1,111 @@
+/*
+ * MasterEffectView.cpp
+ *
+ *  Created on: Apr 11, 2021
+ *      Author: jojo
+ */
+
+#include "MasterEffectView.h"
+#include "SoundEngineView.h"
+
+MasterEffectView::MasterEffectView() {
+	// TODO Auto-generated constructor stub
+
+}
+
+MasterEffectView::~MasterEffectView() {
+	// TODO Auto-generated destructor stub
+}
+
+Scene MasterEffectView::create(Frame &frame) {
+	std::vector<Control*> controls;
+
+	ActionHandler& handler = frame.cube.action_handler;
+
+	//Background
+	Pane* bg = new Pane(sf::Color(80, 80, 80), 0, 0, frame.get_width(), frame.get_height());
+	controls.push_back(bg);
+
+	SoundEngineDevice& sound_engine = frame.cube.engine;
+
+	//Channels
+	int rows = 2;
+	int cols = SOUND_ENGINE_MASTER_EFFECT_AMOUNT / rows;
+	int pane_width = (frame.get_width() - 15) / cols;
+	int pane_height = (frame.get_height() - 50 - 5) / rows;
+	for (size_t i = 0; i < SOUND_ENGINE_MASTER_EFFECT_AMOUNT; ++i) {
+		MasterEffect& effect = sound_engine.effects[i];
+		//Background pane
+		int x = 10 + pane_width * (i % cols);
+		int y = 10 + pane_height * (i / cols);
+		Pane* pane = new Pane(sf::Color(120, 120, 120), x, y, pane_width - 5, pane_height - 5);
+
+		controls.push_back(pane);
+
+		//Title
+		Label* title = new Label("Channel " + std::to_string(i + 1), main_font, 16, x + 5, y + 5);
+		controls.push_back(title);
+
+		//Active
+		CheckBox* active = new CheckBox(false, "", main_font, 12, x + pane_width - 30, y + 5, 20, 20);
+		active->property.bind_function<bool>(std::bind(&SoundEngineChannel::is_active, &channel), std::bind(&SoundEngineChannel::set_active, &channel, std::placeholders::_1), handler);
+		controls.push_back(active);
+		//Engine
+		Button* engine = new Button("Engine", main_font, 12, x + 5, y + 30,  pane_width - 15, 30);
+		engine->rect.setFillColor(sf::Color(0, 180, 255));
+		controls.push_back(engine);
+		engine_buttons[i] = engine;
+		engine->set_on_click([&channel, i, &frame]() {
+			frame.change_view(new SoundEngineChannelView(channel, i));
+		});
+
+		handler.queue_action(new GetFunctionAction<ssize_t, ssize_t>(std::bind(&SoundEngineChannel::get_engine_index, &channel), [this, i](ssize_t index) {
+			engine_buttons[i]->update_text(index < 0 ? "None" : engine_names.at(index));
+		}));
+
+		//Volume
+		Slider* volume = new Slider(0, 0, 1, main_font, x + (pane_width - 5)/2 - 20, y + 70, 40, 180);
+		volume->property.bind(channel.volume, handler);
+		controls.push_back(volume);
+	}
+
+	//Metronome
+	CheckBox* metronome = new CheckBox(false, "Metronome", main_font, 18, 10, frame.get_height() - 45, 40, 40);
+	metronome->property.bind(engine->play_metronome, handler);
+	controls.push_back(metronome);
+
+	DragBox<int>* bpm = new DragBox<int>(120, 10, 480, main_font, 18, 200, frame.get_height() - 45, 100, 40);
+	bpm->drag_mul = 0.00125;
+	bpm->property.bind_function<unsigned int>(std::bind(&Metronome::get_bpm, &engine->metronome), std::bind(&Metronome::set_bpm, &engine->metronome, std::placeholders::_1), handler);
+	controls.push_back(bpm);
+
+	//Volume
+	DragBox<double>* volume = new DragBox<double>(0, 0, 1, main_font, 18, 330, frame.get_height() - 45, 100, 40);
+	volume->property.bind(engine->volume, handler);
+	controls.push_back(volume);
+
+	//Scene
+	Button* scene = new Button("Scenes", main_font, 18, frame.get_width() - 270, frame.get_height() - 40, 100, 40);
+	scene->set_on_click([&frame]() {
+		frame.change_view(new SceneView());
+	});
+	controls.push_back(scene);
+
+	//Program Button
+	Button* program = new Button("Programs", main_font, 18, frame.get_width() - 170, frame.get_height() - 40, 100, 40);
+	program->set_on_click([&frame]() {
+		frame.change_view(new ProgramView());
+	});
+	controls.push_back(program);
+
+	//Back Button
+	Button* exit = new Button("Back", main_font, 18, frame.get_width() - 70, frame.get_height() - 40, 70, 40);
+	exit->set_on_click([&frame]() {
+		frame.change_view(new SoundEngineView());
+	});
+	exit->rect.setFillColor(sf::Color::Yellow);
+	controls.push_back(exit);
+
+
+	return {controls};
+}
