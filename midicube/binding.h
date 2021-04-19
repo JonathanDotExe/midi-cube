879b7b83bf76fdaa4b79ae51d16332b1a0e100d9
JonathanDotExe <jhocheneder@gmx.at>
2021-04-19 15:23:54 +0200

    Added binding.h

---
+++ midicube/binding.h
@@ -0,0 +1,23 @@
+/*
+ * binding.h
+ *
+ *  Created on: 19 Apr 2021
+ *      Author: jojo
+ */
+
+#ifndef MIDICUBE_BINDING_H_
+#define MIDICUBE_BINDING_H_
+
+class ControlBinding {
+public:
+	virtual void on_bind();
+};
+
+class MidiControlHandler {
+public:
+
+};
+
+
+
+#endif /* MIDICUBE_BINDING_H_ */
