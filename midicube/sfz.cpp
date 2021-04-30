a5ef0e4f465aa795659f77fb1d35c3565140005a
JonathanDotExe <jhocheneder@gmx.at>
2021-04-30 18:23:28 +0200

    Finished sfz parser algorithm

--- midicube/sfz.cpp
+++ midicube/sfz.cpp
@@ -11,7 +11,7 @@
 #include <iostream>
 
 enum ParserMode {
-	GLOBAL, GROUP, REGION
+	NONE, GLOBAL, GROUP, REGION
 };
 
 SfzInstrument SfzParser::parse(std::string text) {
@@ -33,11 +33,51 @@ SfzInstrument SfzParser::parse(std::string text) {
 	}
 
 	//Parse lines
+	ParserMode mode = NONE;
 	for (std::string token : tokens) {
 		boost::trim(token);
 		if (token.size() != 0) {
-			//TODO
-			std::cout << token << std::endl;
+			//Change header
+			if (token == "<group>") {
+				mode = GROUP;
+				instrument.groups.push_back({});
+			}
+			else if (token == "<region>") {
+				mode = REGION;
+				if (instrument.groups.size() == 0) {
+					instrument.groups.push_back({});
+				}
+				instrument.groups.at(instrument.groups.size() - 1).regions.push_back({});
+			}
+			else if (token == "<global>") {
+				mode = GLOBAL;
+			}
+			//Attribute
+			else {
+				size_t index = token.find("=");
+				if (index >= token.size()) {
+					std::string opcode = token.substr(0, index);
+					std::string value = token.substr(index + 1);
+
+					switch (mode) {
+					case NONE:
+						std::cout << "No header has been found yet. Ignoring opcode \"" << token << "\"." << std::endl;
+						break;
+					case GLOBAL:
+						instrument.global[opcode] = value;
+						break;
+					case GROUP:
+						instrument.groups.at(instrument.groups.size() - 1).opcodes[opcode] = value;
+						break;
+					case REGION:
+						instrument.groups.at(instrument.groups.size() - 1).regions.at(instrument.groups.at(instrument.groups.size() - 1).regions.size() - 1).opcodes[opcode] = value;
+						break;
+					}
+				}
+				else {
+					std::cout << "Ignoring unrecognized token \"" << token << "\"" << std::endl;
+				}
+			}
 		}
 	}
 
