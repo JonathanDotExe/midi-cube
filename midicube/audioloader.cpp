eaafbeaf03b0bcb57bdb06b76860d80670acb221
JonathanDotExe <jhocheneder@gmx.at>
2021-07-09 21:11:23 +0200

    Implemented streamed audio pool load

--- midicube/audioloader.cpp
+++ midicube/audioloader.cpp
@@ -20,69 +20,6 @@
 #define _countof(arr) (sizeof(arr)/sizeof(arr[0]))
 #endif
 
-StreamedAudioLoader global_audio_loader;
-
-void StreamedAudioLoader::queue_request(LoadRequest request) {
-	requests.push(request);
-	std::cout << "Requested loading block " << request.block << " in " << request.buffer_index << " of " << request.sample->path << std::endl;
-}
-
-void StreamedAudioLoader::run() {
-	using namespace std::chrono_literals;
-	std::cout << "Started loading thread" << std::endl;
-	while (running) {
-		LoadRequest req;
-		if (requests.pop(req)) {
-			std::cout << "Popping request" << std::endl;
-			(*req.buffer)[req.buffer_index].lock.lock();
-			std::cout << "Started loading block" << std::endl;
-			if ((*req.buffer)[req.buffer_index].content_id != req.block) {
-				//Assumes that file doesn't change over usage
-				//Open
-				SNDFILE* file = nullptr;
-				SF_INFO info;
-
-				file = sf_open(req.sample->path.c_str(), SFM_READ, &info);
-
-				//Read
-				if (file != nullptr) {
-					const size_t frames = req.buffer->size / req.sample->channels;
-					if (sf_seek(file, frames * req.block, SF_SEEK_SET) != -1) {
-						float* buffer = (*req.buffer)[req.buffer_index].buffer;
-						//memset((void *) buffer, 0, size * sizeof(buffer[0]));
-						sf_readf_float(file, buffer, frames);	//It must be externally ensured that all the frames fit in the buffer
-						(*req.buffer)[req.buffer_index].content_id = req.block;
-					}
-				}
-				else {
-					std::cerr << "Couldn't open sound file " << req.sample->path << ": " << sf_strerror(file) << std::endl;
-				}
-
-				if (file != nullptr) {
-					sf_close(file);
-					file = nullptr;
-				}
-				(*req.buffer)[req.buffer_index].lock.unlock();
-				std::cout << "Loaded block " << req.block << " in " << req.buffer_index << " of " << req.sample->path << std::endl;
-			}
-		}
-		else {
-			std::this_thread::sleep_for(1ms);
-		}
-	}
-}
-
-void StreamedAudioLoader::start() {
-	for (size_t i = 0; i < 4; ++i) {
-		std::thread thread([this]() { run(); });
-		thread.detach();
-	}
-}
-
-void StreamedAudioLoader::stop() {
-	running = false;
-}
-
 StreamedAudioSample* StreamedAudioPool::load_sample(std::string fname) {
 	samples.push_back({});
 	StreamedAudioSample& sample = samples[samples.size() - 1];
@@ -95,43 +32,45 @@ void StreamedAudioPool::queue_request(LoadRequest request) {
 }
 
 void StreamedAudioPool::run() {
-	//TODO
 	using namespace std::chrono_literals;
-	std::cout << "Started loading thread" << std::endl;
+	std::cout << "Started sample loading thread ..." << std::endl;
 	while (running) {
 		LoadRequest req;
 		if (requests.pop(req)) {
-			std::cout << "Popping request" << std::endl;
-			(*req.buffer)[req.buffer_index].lock.lock();
-			std::cout << "Started loading block" << std::endl;
-			if ((*req.buffer)[req.buffer_index].content_id != req.block) {
-				//Assumes that file doesn't change over usage
-				//Open
-				SNDFILE* file = nullptr;
-				SF_INFO info;
+			//Assumes that files don't change
+			//Open
+			SNDFILE* file = nullptr;
+			SF_INFO info;
+			SF_INSTRUMENT loop;
+			bool success = true;
 
-				file = sf_open(req.sample->path.c_str(), SFM_READ, &info);
+			file = sf_open(req.sample->path.c_str(), SFM_READ, &info);
 
-				//Read
-				if (file != nullptr) {
-					const size_t frames = req.buffer->size / req.sample->channels;
-					if (sf_seek(file, frames * req.block, SF_SEEK_SET) != -1) {
-						float* buffer = (*req.buffer)[req.buffer_index].buffer;
-						//memset((void *) buffer, 0, size * sizeof(buffer[0]));
-						sf_readf_float(file, buffer, frames);	//It must be externally ensured that all the frames fit in the buffer
-						(*req.buffer)[req.buffer_index].content_id = req.block;
-					}
+			//Read
+			if (file != nullptr) {
+				req.sample->lock.lock();
+				if (!req.sample->loaded) {
+					float buffer[1024];
+					sf_count_t size = _countof(buffer);
+					sf_count_t count;
+					do {
+						count = sf_read_float(file, buffer, size);
+						for (sf_count_t i = 0; i < count; i++) {
+							req.sample->samples.push_back(buffer[i]);
+						}
+					} while (count >= size);
+					req.sample->loaded = true;
 				}
-				else {
-					std::cerr << "Couldn't open sound file " << req.sample->path << ": " << sf_strerror(file) << std::endl;
-				}
+				req.sample->lock.unlock();
+			}
+			else {
+				std::cerr << "Couldn't open sound file " << req.sample->path << ": " << sf_strerror(NULL) << std::endl;
+				success = false;
+			}
 
-				if (file != nullptr) {
-					sf_close(file);
-					file = nullptr;
-				}
-				(*req.buffer)[req.buffer_index].lock.unlock();
-				std::cout << "Loaded block " << req.block << " in " << req.buffer_index << " of " << req.sample->path << std::endl;
+			if (file != nullptr) {
+				sf_close(file);
+				file = nullptr;
 			}
 		}
 		else {
--- midicube/audioloader.h
+++ midicube/audioloader.h
@@ -15,37 +15,8 @@
 
 
 struct LoadRequest {
-	MultiBuffer<float>* buffer;
-	size_t buffer_index;
 	StreamedAudioSample* sample;
-	size_t block;
 };
-
-class StreamedAudioLoader {
-private:
-	boost::lockfree::queue<LoadRequest> requests;
-	std::atomic<bool> running{true};
-	std::array<std::thread, 4> worker;
-
-public:
-
-	StreamedAudioLoader() : requests(1024) {
-
-	}
-
-	//Audio Thread
-	void queue_request(LoadRequest request);
-	void run();
-	void stop();
-	void start();
-
-	~StreamedAudioLoader() {
-		stop();
-	}
-
-};
-
-extern StreamedAudioLoader global_audio_loader; //FIXME
 
 class StreamedAudioPool {
 private:
