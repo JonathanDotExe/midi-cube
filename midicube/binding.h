0dfc66b0ec2862af51b60d13a72f8d921d3cc0c2
JonathanDotExe <jhocheneder@gmx.at>
2021-07-16 18:25:29 +0200

    Added BindibleValue

--- midicube/binding.h
+++ midicube/binding.h
@@ -12,6 +12,66 @@
 #include <unordered_map>
 #include "midi.h"
 
+template<typename T>
+class BindableValue {
+private:
+	T persistent_value;
+	T temp_value;
+
+	T total_min;
+	T total_max;
+
+	T temp_minchange = 1;
+
+	double cc_val = 0;
+
+	inline void recalc_temp() {
+		temp_value = fmin(fmax(total_min, persistent_value + (total_max - total_min) * cc_val), total_max);
+	}
+
+public:
+
+	unsigned int persistent_cc = 128;
+	unsigned int temp_cc = 128;
+
+	BindableValue(T val = 0, T min = 0, T max = 0) {
+		this->persistent_value = val;
+		this->total_min = min;
+		this->total_max = max;
+	}
+
+	inline T& operator=(const T& other) {
+		persistent_value = other;
+		temp_value = other;
+		return persistent_value;
+	}
+
+	inline operator T() const {
+		return temp_value;
+	}
+
+	inline const T get_persistent() const {
+		return persistent_value;
+	}
+
+	bool control_change(unsigned int control, double val) {
+		bool changed = false;
+		if (control == persistent_cc) {
+			persistent_value = total_min + (total_max - total_min) * val;
+			changed = true;
+		}
+		if (control == temp_cc) {
+			cc_val = val;
+			changed = true;
+		}
+		if (changed) {
+			recalc_temp();
+		}
+		return changed;
+	}
+
+};
+
 class ControlBinding {
 public:
 	unsigned int cc = 0;
