09b683b4bc94e5f7a0f825e14e8597b5c9a9e3c4
JonathanDotExe <jhocheneder@gmx.at>
2021-10-17 13:49:10 +0200

    Removed action system

--- midicube/framework/data/data.h
+++ midicube/framework/data/data.h
@@ -15,187 +15,6 @@
 #include "../util/util.h"
 #include "binding.h"
 
-class Action {
-
-public:
-
-	virtual void execute() = 0;
-
-	virtual void returned() = 0;
-
-	virtual ~Action() {
-
-	}
-};
-
-class FunctionAction : public Action {
-
-private:
-	std::function<void()> exec;
-	std::function<void()> ret;
-
-public:
-
-	FunctionAction(std::function<void()> e = nullptr, std::function<void()> r = nullptr);
-
-	virtual void execute();
-
-	virtual void returned();
-
-	virtual ~FunctionAction() {
-
-	}
-};
-
-template <typename T, typename V>
-class GetValueAction : public Action{
-
-private:
-	T& t;
-	V v;
-	std::function<void(V)> callback;
-
-public:
-
-	GetValueAction(T& var, std::function<void(V)> c) : t(var), callback(c) {
-
-	}
-
-	virtual void execute() {
-		v = t;
-	}
-
-	virtual void returned() {
-		callback(v);
-	}
-
-	virtual ~GetValueAction() {
-
-	}
-};
-
-template <typename T, typename V>
-class GetFunctionAction : public Action{
-
-private:
-	std::function<T ()> get_func;
-	V v;
-	std::function<void(V)> callback;
-
-public:
-
-	GetFunctionAction(std::function<T ()> f, std::function<void(V)> c) : get_func(f), callback(c) {
-
-	}
-
-	virtual void execute() {
-		v = get_func();
-	}
-
-	virtual void returned() {
-		callback(v);
-	}
-
-	virtual ~GetFunctionAction() {
-
-	}
-};
-
-template <typename T, typename V>
-class SetValueAction : public Action {
-
-private:
-	T& t;
-	V v;
-
-public:
-
-	SetValueAction(T& var, V val) : t(var), v(val) {
-
-	}
-
-	virtual void execute() {
-		t = v;
-	}
-
-	virtual void returned() {
-
-	}
-
-	virtual ~SetValueAction() {
-
-	}
-
-};
-
-template <typename T, typename V>
-class SetValueCastAction : public Action {
-
-private:
-	T& t;
-	V v;
-
-public:
-
-	SetValueCastAction(T& var, V val) : t(var), v(val) {
-
-	}
-
-	virtual void execute() {
-		t = static_cast<T>(v);
-	}
-
-	virtual void returned() {
-
-	}
-
-	virtual ~SetValueCastAction() {
-
-	}
-
-};
-
-template <typename T, typename V>
-class SetFunctionAction : public Action {
-
-private:
-	std::function<void (T)> set_func;
-	V v;
-
-public:
-
-	SetFunctionAction(std::function<void (T)> f, V val) : set_func(f), v(val) {
-
-	}
-
-	virtual void execute() {
-		set_func(static_cast<T>(v));
-	}
-
-	virtual void returned() {
-
-	}
-
-	virtual ~SetFunctionAction() {
-
-	}
-};
-
-class ActionHandler {
-private:
-	boost::lockfree::queue<Action*> realtime_actions;
-	boost::lockfree::queue<Action*> return_actions;
-public:
-	ActionHandler();
-	bool remaining_realtime_actions();
-	bool remaining_return_actions();
-	void queue_action(Action* action);
-	void execute_realtime_actions();
-	void execute_return_actions();
-};
-
-
-
 template <typename T>
 class PropertyBinding {
 private:
@@ -223,41 +42,39 @@ public:
 	}
 
 	template <typename E>
-	void bind(E& e, ActionHandler& handler) {
-		get_func = [&e, &handler](std::function<void (T)> callback) {
-			handler.queue_action(new GetValueAction<E, T>(e, callback));
+	void bind(E& e, SpinLock& lock) {
+		get_func = [&e, &lock](std::function<void (T)> callback) {
+			lock.lock();
+			callback(e);
+			lock.unlock();
 		};
-		set_func = [&e, &handler](T t) {
-			handler.queue_action(new SetValueAction<E, T>(e, t));
+		set_func = [&e, &lock](T t) {
+			lock.lock();
+			e = t;
+			lock.unlock();
 		};
+
 		if (std::is_base_of<BindableValue, E>()) {
 			object = (BindableValue*) (&e); //FIXME
 		}
 	}
 
 	template <typename E>
-	void bind_cast(E& e, ActionHandler& handler) {
-		get_func = [&e, &handler](std::function<void (T)> callback) {
-			handler.queue_action(new GetValueAction<E, T>(e, callback));
+	void bind_cast(E& e, SpinLock& lock) {
+		get_func = [&e, &lock](std::function<void (T)> callback) {
+			lock.lock();
+			callback(e);
+			lock.unlock();
 		};
-		set_func = [&e, &handler](T t) {
-			handler.queue_action(new SetValueCastAction<E, T>(e, t));
+		set_func = [&e, &lock](T t) {
+			lock.lock();
+			e = static_cast<E>(t);
+			lock.unlock();
 		};
 
 		if (std::is_base_of<BindableValue, E>()) {
 			object = (BindableValue*)(&e); //FIXME
 		}
-	}
-
-	template <typename E>
-	void bind_function(std::function<E ()> get, std::function<void (E)> set, ActionHandler& handler) {
-		get_func = [get, &handler](std::function<void (T)> callback) {
-			handler.queue_action(new GetFunctionAction<E, T>(get, callback));
-		};
-		set_func = [set, &handler](T t) {
-			handler.queue_action(new SetFunctionAction<E, T>(set, t));
-		};
-		object = nullptr;
 	}
 
 	template <typename E>
--- midicube/framework/gui/core.cpp
+++ midicube/framework/gui/core.cpp
@@ -40,7 +40,7 @@ void Control::set_visible(bool visible) {
 }
 
 //Frame
-Frame::Frame(MidiCube& c, int width, int height, std::string title, bool render_sleep) : cube(c) {
+Frame::Frame(int width, int height, std::string title, bool render_sleep) {
 	this->width = width;
 	this->height = height;
 	this->title = title;
@@ -63,15 +63,13 @@ void Frame::run(ViewController* v) {
 
 	while (window.isOpen()) {
 		//Property changes
-		if (cube.updated) {
-			for (Control* control : controls) {
-				control->update_properties();
-			}
-			view->update_properties();
-			cube.updated = false;
+		/*
+		TODO property updates
+		for (Control* control : controls) {
+			control->update_properties();
 		}
-		//Execute return actions
-		cube.action_handler.execute_return_actions();
+		view->update_properties();
+		*/
 		//Events
 		sf::Event event;
 		while (window.pollEvent(event)) {
@@ -149,14 +147,6 @@ void Frame::run(ViewController* v) {
 		window.display();
 		//Change view
 		if (next_view) {
-			//Wait for tasks
-			while (cube.action_handler.remaining_realtime_actions()) {
-				std::this_thread::yield();
-			}
-			while (cube.action_handler.remaining_return_actions()) {
-				cube.action_handler.execute_return_actions();
-				std::this_thread::yield();
-			}
 			switch_view(next_view);
 			next_view = nullptr;
 		}
