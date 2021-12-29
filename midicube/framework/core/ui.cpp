16dfcff7067a5e4026fb46dc9e080004f39309a6
JonathanDotExe <jhocheneder@gmx.at>
2021-12-29 16:06:44 +0100

    Fixed control view not changing

--- midicube/framework/core/ui.cpp
+++ midicube/framework/core/ui.cpp
@@ -12,21 +12,23 @@ MenuHandler::MenuHandler() {
 }
 
 void MenuHandler::change_menu(Menu *menu, bool append_history) {
-	if (append_history && this->curr_menu) {
-		history.push_back(curr_menu);
-	}
-	else {
-		delete curr_menu;
-	}
+	if (menu) {
+		if (append_history && this->curr_menu) {
+			history.push_back(curr_menu);
+		}
+		else {
+			delete curr_menu;
+		}
 
-	curr_menu = menu;
-	ViewController* gui = curr_menu->create_gui_view();
-	if (view_host && gui) {
-		view_host->change_view(gui);
-	}
-	ControlView* control = curr_menu->create_control_view();
-	if (control_host && control) {
-		control_host->change_control_view(control);
+		curr_menu = menu;
+		ViewController* gui = curr_menu->create_gui_view();
+		if (view_host) {
+			view_host->change_view(gui);
+		}
+		ControlView* control = curr_menu->create_control_view();
+		if (control_host) {
+			control_host->change_control_view(control);
+		}
 	}
 }
 
