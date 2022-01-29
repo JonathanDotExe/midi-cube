374d48938840ca5bfbf8569c896d132e5ae1c6d0
JonathanDotExe <jhocheneder@gmx.at>
2022-01-29 21:56:45 +0100

    Implemented new enum parameter

--- midicube/framework/core/control.h
+++ midicube/framework/core/control.h
@@ -178,60 +178,22 @@ public:
 template<typename T>
 class TemplateEnumParameter : public ITemplateParameter<T> {
 private:
-	T value;
-
-public:
-
+	T& variable;
 	T total_min;
 	T total_max;
+
+public:
 
 	void* get_property() {
 		return this;
 	}
 
-	TemplateEnumParameter(T val, T min, T max) : total_min(min), total_max(max) {
-		this->value = val;
-	}
+	TemplateEnumParameter(T& v, T min, T max) : variable(v){
 
-	TemplateEnumParameter(const TemplateParameter<T>& other) {
-		this->value = other.value;
-		this->total_min = other.total_min;
-		this->total_max = other.total_max;
-	}
-
-	inline TemplateParameter<T>& operator=(const TemplateParameter<T>& other) {
-		if (&other != this) {
-			this->value = other.value;
-			this->total_min = other.total_min;
-			this->total_max = other.total_max;
-		}
-		return *this;
-	}
-
-	inline T& operator=(const T& other) {
-		this->value = other;
-		return value;
-	}
-
-	inline int operator=(const int& other) {
-		this->value = static_cast<T>(other);
-		return other;
-	}
-
-	inline operator T() const {
-		return value;
-	}
-
-	inline operator T&() {
-		return value;
-	}
-
-	inline operator int() const {
-		return static_cast<int>(value);
 	}
 
 	void change(double val) {
-		value = static_cast<T>(static_cast<int>(total_min) + (static_cast<int>(total_max) - static_cast<int>(total_min)) * val);
+		set(static_cast<T>(static_cast<int>(total_min) + (static_cast<int>(total_max) - static_cast<int>(total_min)) * val));
 	}
 
 	T get_min() const {
@@ -240,6 +202,14 @@ public:
 
 	T get_max() const {
 		return total_max;
+	}
+
+	T get() {
+		return variable;
+	}
+
+	void set(T val) {
+		variable = val;
 	}
 
 };
